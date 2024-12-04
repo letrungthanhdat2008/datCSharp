3b53c537b890b06b4685db4f8ecb8814d76941d5
letrungthanhdat2008 <letrungthanhdat6@gmail.com>
2024-12-04 17:49:25 +0700

    Add files via upload

---
+++ tinhtong2songuyen.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("\nNhap so thu nhat: ");
+        int num1 = int.Parse(Console.ReadLine());
+
+        Console.Write("\nNhap so thu hai: ");
+        int num2 = int.Parse(Console.ReadLine());
+
+        int sum = num1 + num2;
+
+        Console.WriteLine($"Tong cua hai so la:{sum}");
+    }
+}
